6a6a2ab48a14aa600aee69a604ddbdf2a184c1cd
dip2002k <kevad2@student.op.ac.nz>
2025-11-07 08:04:14 +1300

    ref app.js

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/posts', require('./routes/postRoutes'));
-app.use('/api/tags', require('./routes/tagRoutes'));
-app.use('/api/comments', require('./routes/commentRoutes'));
-app.use('/api/categories', require('./routes/categoryRoutes'));
 
 // Health check
 app.get('/', (req, res) => {
@@ -17,10 +13,6 @@ app.get('/', (req, res) => {
     message: 'Blog API is running!',
     endpoints: {
       users: '/api/users',
-      posts: '/api/posts',
-      tags: '/api/tags', 
-      comments: '/api/comments',
-      categories: '/api/categories'
     }
   });
 });
